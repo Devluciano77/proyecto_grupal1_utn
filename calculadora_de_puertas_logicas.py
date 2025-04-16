91af02598a321a68a079843d73f158082cfec6c7
Devluciano77 <lmrottoli301097@gmail.com>
2025-04-16 09:52:55 -0300

    feat: funciones del programa

--- calculadora_de_puertas_logicas.py
+++ calculadora_de_puertas_logicas.py
@@ -1,10 +1,14 @@
 # -- Calculadora de puertas lógicas -- 
 
 # Funciones de las puertas logicas
-
+def AND(num1, num2):
+    return num1 & num2
+def OR(num1, num2):
+    return num1 | num2
 
 # Pedir los valores
-
+print("=== Calculadora de Puertas Lógicas ===")
+print("Solo puedes ingresar 0 o 1")
 
 # Menú de operaciones
 
