914e4329cd7f39dfac6130c99194d61f16d5018f
JustoBellingi <jbellingi2006@gmail.com>
2025-04-18 09:22:52 -0300

    Update calculadora_de_puertas_logicas.py

--- calculadora_de_puertas_logicas.py
+++ calculadora_de_puertas_logicas.py
@@ -1,5 +1,12 @@
 # -- Calculadora de puertas lógicas -- 
-
+def ingresar_NumBinario (num):
+    while True:
+        valor = input(num)
+        if valor == "0" or valor == "1":
+            # checkea que el valor sea un numero binario
+            return int(valor)
+        else:
+            print("Por favor, ingresa un numero binario que sea 1 o 0")
 # Funciones de las puertas logicas
 def AND(num1, num2):
     return num1 & num2
@@ -18,10 +25,43 @@ print("=== Calculadora de Puertas Lógicas ===")
 print("Solo puedes ingresar 0 o 1")
 
 # los print son de prueba para las funciones
-print(XOR(1,0))
-print(XOR(1,1))
-print(XOR(0,1))
-print(XOR(0,0))
+while True:
+  print("Opciones disponibles:")
+  print("1. AND")
+  print("2. OR")
+  print("3. NOT")
+  print("4. NAND")
+  print("5. XOR")
+  print("6. NOR")
+  print("7. Salir")
+  opcion = input("Selecciona una opción (1-7): ")
+  if opcion == '1'or opcion == '2'or opcion ==  '4' or opcion == '5' or opcion ==  '6': 
+      # si esta dentro de estas opciones
+      num1 = ingresar_NumBinario("Ingresa el primer número binario: ")
+      num2 = ingresar_NumBinario("Ingresa el segundo número binario: ")
+  if opcion == '3': 
+      # si la opcion elegida es la 3
+      num = ingresar_NumBinario("Ingresa un número binario (0 o 1): ")
+      print(f"Resultado de la puerta not: {NOT(num)}")
+  if opcion == '7':
+      # si la opcion elegida es la 7
+      print("¡Chau, te esperamos para que vuelvas!")
+      break
+  if opcion == "1":
+     print(f"Resultado de la puerta AND:{AND (num1, num2)}")
+  elif opcion == '2':
+     print(f"Resultado de la puerta OR: {OR (num1, num2)}")
+  elif opcion == '4':
+     print(f"Resultado de la puerta NAND: {NAND(num1, num2)}")
+  elif opcion == '5':
+     print(f"Resultado de la puerta XOR: {XOR(num1, num2)}")
+  elif opcion == '6':
+     print(f"Resultado de la puertaNOR: {NOR(num1, num2)}")
+# Menú de operaciones
+
+
+# Procesar la operación
+
 # Menú de operaciones
 
 
