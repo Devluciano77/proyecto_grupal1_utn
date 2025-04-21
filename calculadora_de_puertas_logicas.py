bc5a7196e22799f57e4508e2eb7eeb6f9f903e0d
Devluciano77 <lmrottoli301097@gmail.com>
2025-04-21 15:43:18 -0300

    fix: simplificacion de codigo

--- calculadora_de_puertas_logicas.py
+++ calculadora_de_puertas_logicas.py
@@ -1,4 +1,6 @@
 # -- Calculadora de puertas lógicas -- 
+
+# ingreso de datos binarios
 def ingresar_NumBinario (num):
     while True:
         valor = input(num)
@@ -7,6 +9,8 @@ def ingresar_NumBinario (num):
             return int(valor)
         else:
             print("Por favor, ingresa un numero binario que sea 1 o 0")
+
+
 # Funciones de las puertas logicas
 def AND(num1, num2):
     return num1 & num2
@@ -20,11 +24,10 @@ def XOR(num1, num2):
     return num1 ^ num2
 def NOR(num1, num2):
     return 1 - (num1 | num2)
-# Pedir los valores
-print("=== Calculadora de Puertas Lógicas ===")
-print("Solo puedes ingresar 0 o 1")
 
-# los print son de prueba para las funciones
+print("=== Calculadora de Puertas Lógicas ===")
+
+# menu de opciones de puertas logicas 
 while True:
   print("Opciones disponibles:")
   print("1. AND")
@@ -34,13 +37,18 @@ while True:
   print("5. XOR")
   print("6. NOR")
   print("7. Salir")
+  
   opcion = input("Selecciona una opción (1-7): ")
-  if opcion == '1'or opcion == '2'or opcion ==  '4' or opcion == '5' or opcion ==  '6': 
+
+  # proceso de la informacion
+  if opcion in ("1","2","4","5","6"): 
       # si esta dentro de estas opciones
+      print("Solo puedes ingresar 0 o 1")
       num1 = ingresar_NumBinario("Ingresa el primer número binario: ")
       num2 = ingresar_NumBinario("Ingresa el segundo número binario: ")
   if opcion == '3': 
       # si la opcion elegida es la 3
+      print("Solo puedes ingresar 0 o 1")
       num = ingresar_NumBinario("Ingresa un número binario (0 o 1): ")
       print(f"Resultado de la puerta not: {NOT(num)}")
   if opcion == '7':
@@ -57,12 +65,3 @@ while True:
      print(f"Resultado de la puerta XOR: {XOR(num1, num2)}")
   elif opcion == '6':
      print(f"Resultado de la puertaNOR: {NOR(num1, num2)}")
-# Menú de operaciones
-
-
-# Procesar la operación
-
-# Menú de operaciones
-
-
-# Procesar la operación
